fdda68ade5a2b760f5432008ebbe6665e8560877
Shubham Jaswal <shubhamjaswal772@gmail.com>
2019-03-10 00:28:28 +0530

    admin changes

--- payments/admin.py
+++ payments/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
-from payments.models import PaytmHistory
-# Register your models here.
-class PaytmHistoryAdmin(admin.ModelAdmin):
-    list_display = ('user', 'MID', 'TXNAMOUNT', 'STATUS')
+from .models import Paytm_history
+# from payments.models import PaytmHistory
+# # Register your models here.
+# class PaytmHistoryAdmin(admin.ModelAdmin):
+#     list_display = ('user', 'MID', 'TXNAMOUNT', 'STATUS')
+#
+#
+admin.site.register(Paytm_history)
 
-
-admin.site.register(PaytmHistory, PaytmHistoryAdmin)
+# PaytmHistoryAdmin)
