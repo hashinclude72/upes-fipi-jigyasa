33443e5039900f00f08a794fc6f217e75391ecfa
Shubham Jaswal <shubhamjaswal772@gmail.com>
2019-03-10 16:37:43 +0530

    -----

--- fipi/settings.py
+++ fipi/settings.py
@@ -61,7 +61,7 @@ ROOT_URLCONF = 'fipi.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ["templates"],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
