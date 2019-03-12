ad1167346f1ed8a1efc749d1d6a42ded019ce4b3
Shubham Jaswal <shubhamjaswal772@gmail.com>
2019-03-13 01:08:27 +0530

    update settings

--- fipi/settings.py
+++ fipi/settings.py
@@ -27,7 +27,9 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 #DEBUG = True
 DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
 
-ALLOWED_HOSTS = ['upesjigyasa.herokuapp.com',]
+ALLOWED_HOSTS = ['upesjigyasa.herokuapp.com',
+                'www.upesjigyasa.com',
+                ]
 
 
 # Application definition
