6c53dc878003d8d73eb93fba80d7e3dd3d4bf15c
Shubham Jaswal <shubhamjaswal772@gmail.com>
2019-03-24 00:48:52 +0530

    migrations

--- users/migrations/0006_auto_20190324_0020.py
+++ users/migrations/0006_auto_20190324_0020.py
@@ -12,12 +12,7 @@ class Migration(migrations.Migration):
     operations = [
         migrations.AddField(
             model_name='user_details',
-            name='referral',
-            field=models.CharField(blank=True, max_length=30, null=True, verbose_name='REFERRAL'),
-        ),
-        migrations.AlterField(
-            model_name='user_details',
             name='status',
-            field=models.CharField(default='Not Paid', max_length=12, verbose_name='STATUS'),
+            field=models.CharField(default='Not set', max_length=12, verbose_name='STATUS'),
         ),
     ]
