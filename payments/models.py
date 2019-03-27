6221c772c5ec3188312ce2b1fe44133565d14f4a
Shubham Jaswal <shubhamjaswal772@gmail.com>
2019-03-27 23:11:13 +0530

    charfeild banktrnx

--- payments/models.py
+++ payments/models.py
@@ -9,7 +9,7 @@ class Paytm_history(models.Model):
     ORDERID = models.CharField('ORDER ID', max_length=30)
     TXNDATE = models.DateTimeField('TXN DATE', default=timezone.now)
     TXNID = models.CharField('TXN ID', max_length=100)
-    BANKTXNID = models.IntegerField('BANK TXN ID', null=True, blank=True)
+    BANKTXNID = models.CharField('BANK TXN ID',max_length=100, null=True, blank=True)
     BANKNAME = models.CharField('BANK NAME', max_length=50, null=True, blank=True)
     RESPCODE = models.IntegerField('RESP CODE')
     PAYMENTMODE = models.CharField('PAYMENT MODE', max_length=10, null=True, blank=True)
